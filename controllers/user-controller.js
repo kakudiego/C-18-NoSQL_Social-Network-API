dbc781e1c3a9ddee2c7f87e8930810cd3814ce6b
Diego Kaku <diegokaku@gmail.com>
2022-04-30 02:37:15 -0500

    controllers: user

---
+++ controllers/user-controller.js
@@ -0,0 +1,79 @@
+const { User } = require('../models');
+
+const UserController = {
+  // get all Users
+  getAllUser(req, res) {
+    User.find({})
+      .populate({
+        path: 'thoughts',
+        select: '-__v',
+      })
+      .select('-__v')
+      .sort({ _id: -1 })
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  },
+
+  // get one User by id
+  getUserById({ params }, res) {
+    User.findOne({ _id: params.id })
+      .populate({
+        path: 'thoughts',
+        select: '-__v',
+      })
+      .select('-__v')
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  },
+
+  // createUser
+  createUser({ body }, res) {
+    User.create(body)
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => res.json(err));
+  },
+
+  // update User by id
+  updateUser({ params, body }, res) {
+    User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No User found with this id!' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.json(err));
+  },
+
+  // delete User
+  deleteUser({ params }, res) {
+    User.findOneAndDelete({ _id: params.id })
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => res.json(err));
+  },
+
+  // add a new friend to a user's friends list
+  addFriend({ params, body }, res) {
+    User.findOneAndUpdate({ _id: params.id }, { $push: { friends: params.friendId } }, { new: true });
+    then((dbUserData) => {
+      res.json(dbUserData);
+    }).catch((err) => res.json(err));
+  },
+
+  // remove a friend from a user's friends list
+  removeFriend({ params }, res) {
+    User.findOneAndUpdate({ _id: params.id }, { $pull: { friends: params.friendId } }, { new: true });
+    then((dbUserData) => {
+      res.json(dbUserData);
+    }).catch((err) => res.json(err));
+  },
+};
+
+module.exports = UserController;
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { getAllPizza, getPizzaById, createPizza, updatePizza, deletePizza } = require('../../controllers/pizza-controller');
+const { getAllPizza, getPizzaById, createPizza, updatePizza, deletePizza } = require('../../controllers/user-controller');
 
 // /api/pizzas
 router.route('/').get(getAllPizza).post(createPizza);

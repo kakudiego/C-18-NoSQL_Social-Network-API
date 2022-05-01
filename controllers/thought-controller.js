0d3e2aa59b57243c2bdea847a70cd0fea5a9d2bf
Diego Kaku <diegokaku@gmail.com>
2022-05-01 00:38:56 -0500

    user/thought get, post, put, delete workinggit add -A!

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -71,22 +71,6 @@ const thoughtController = {
       .catch((err) => res.json(err));
   },
 
-  // createThought({ params, body }, res) {
-  //   Thought.create(body)
-  //     .then(({ _id }) => {
-  //       return User.findOneAndUpdate({ _id: params.userId }, { $push: { thoughts: _id } }, { new: true });
-  //     })
-  //     .then((dbUserData) => {
-  //       console.log(dbUserData);
-  //       if (!dbUserData) {
-  //         res.status(404).json({ message: 'No user found with this id!' });
-  //         return;
-  //       }
-  //       res.json(dbUserData);
-  //     })
-  //     .catch((err) => res.json(err));
-  // },
-
   // update a thought by its _id
   updateThought({ params, body }, res) {
     Thought.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
